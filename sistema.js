2b7f60c35252d2ae4a008ab7a4c7ae4e2c9feeaf
evertonmessias <everton.messias@gmail.com>
2020-01-23 08:54:32 -0300

    23-01-2020_08:54:32

--- sistema.js
+++ sistema.js
@@ -5,7 +5,7 @@ npm install --save express   //framework
 npm install --save sequelize    //acesso ao BD
 npm install --save mysql2   //BD
 npm install --save express-handlebars   //templates
-npm i -D handlebars@4.5.3
+npm install -D handlebars@4.5.3
 npm install --save body-parser  //envio ao BD
 */
 
@@ -15,8 +15,8 @@ const app = express();
 
 //Template Handlebars
 const handlebars = require("express-handlebars");
-app.engine('handlebars',handlebars({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 //Banco de Dados
 const Sequelize = require("sequelize");
@@ -25,50 +25,61 @@ const sequelize = new Sequelize('teste', 'root', '', {
     dialect: 'mysql'
 });
 sequelize.authenticate()
-    .then(() => {console.log("SUCESSO")})
-        .catch(() => {console.log("ERRO")});
+    .then(() => { console.log("SUCESSO") })
+    .catch(() => { console.log("ERRO") });
 
-const cadastro = sequelize.define('cadastro',{
-    nome:{type: Sequelize.STRING},
-    telefone:{type: Sequelize.STRING}
+const cadastro = sequelize.define('cadastro', {
+    nome: { type: Sequelize.STRING },
+    telefone: { type: Sequelize.STRING }
 })//cadastro.sync({force: true}); // aqui cria a tabela
 
 //Body-Parser
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // ******** Rotas ******** 
-app.get("/",(req, res)=>{
+app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/form",(req, res)=>{
+app.get("/form", (req, res) => {
     res.render("form");
 })
 
-app.get("/consulta",(req, res)=>{
-    cadastro.findAll().then((tudo)=>{        
-        res.render("consulta",{tudo: tudo});
-    })    
+app.get("/consulta", (req, res) => {
+    cadastro.findAll().then((tudo) => {
+        res.render("consulta", { tudo: tudo });
+    })
 })
 
-app.get("/apagar",(req, res)=>{
-    res.render("apagar");
-})
-
-app.post("/inserir",(req, res)=>{    
+app.post("/inserir", (req, res) => {
     cadastro.create({
         nome: req.body.nome, telefone: req.body.telefone,
         createdAt: '01/01/2020', updatedAt: '01/01/2020'
-    }).then(function(){
-        res.redirect("consulta");
-    }).catch(function(){
-        res.send("Erro no Formulário!" + erro);     
+    }).then(function () {
+        res.redirect("/consulta");
+    }).catch(function () {
+        res.send("Erro no Formulário!" + erro);
+    })
+})
+
+
+app.get("/apagar", (req, res) => {
+    cadastro.findAll().then((tudo) => {
+        res.render("apagar", { tudo: tudo });
+    })
+})
+
+app.get("/deletar/:id", (req, res) => {
+    cadastro.destroy({ where: { 'id': req.params.id } }).then(() => {
+        res.redirect("/consulta");
+    }).catch(() => {
+        res.send("Erro ao apagar")
     })
 })
 
 // Servidor
-app.listen(8082, ()=>{
+app.listen(8082, () => {
     console.log("Servidor rodando na porta 8082");
 });
